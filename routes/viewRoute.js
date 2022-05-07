6fe7f50d1f028a521fba47605f2a66288ca3e912
Pedro Figueira <pedro.figueirab@hotmail.com>
2022-05-07 14:25:23 -0300

    transpiled ts to js, removed cmd from start script

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -2,7 +2,7 @@
 var Express = require("express");
 var Path = require("path");
 var Router = Express.Router();
-Router.get("/home", function (req, res) {
+Router.get("/", function (req, res) {
     res.sendFile(Path.join(__dirname, "../view/index.html"));
 });
 module.exports = Router;
